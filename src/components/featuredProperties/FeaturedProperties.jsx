b48d9e38a8104f6403dfe46a6c47e17df3556aa3
amh6281 <amh6281@naver.com>
2022-07-08 16:48:47 +0900

    add: 인기 Top의 FeaturedProperties 컴포넌트 추가 2022.07.08

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">나인트리 프리미어 명동</span>
+        <span className="fpCity">명동</span>
+        <span className="fpPrice">123,000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">나인트리 프리미어 명동</span>
+        <span className="fpCity">명동</span>
+        <span className="fpPrice">123,000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">나인트리 프리미어 명동</span>
+        <span className="fpCity">명동</span>
+        <span className="fpPrice">123,000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+          alt=""
+          className="fpImg"
+        />
+        <span className="fpName">나인트리 프리미어 명동</span>
+        <span className="fpCity">명동</span>
+        <span className="fpPrice">123,000</span>
+        <div className="fpRating">
+          <button>8.9</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
@@ -14,6 +15,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">숙소별 찾아보기</h1>
         <PropertyList />
+        <h1 className="homeTitle">인기 Top</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
